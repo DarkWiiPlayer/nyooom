aa48cfe73f2edc18e49941b28e3bed5424169a63
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2025-11-15 20:16:57 +0100

    Add explanation to mini-renderer

--- mini.js
+++ mini.js
@@ -1,3 +1,10 @@
+/* This file is a simplified and self-contained version of nyooom's render function
+ * meant to be copied into projects that need some basic DOM rendering capabilities
+ * with minimal dependency overhead.
+ * It is built to be easily replaced with the full nyooom renderer if the project
+ * outgrows this implementation.
+ */
+
 /**
  * @param {String} name
  * @return {String}
